5338321f0f52b4767e34bfe9a873b95509bbc171
Tom Dailey <tompdailey@gmail.com>
2020-10-03 15:55:56 -0400

    Reformatted names, added getList FromFile

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import csv
-def csv_find_row(file, columnNumber, searchNumber):
+def csvFindRow(file, columnNumber, searchNumber):
 
     csv_f = csv.reader(open(file))
     next(csv_f)
@@ -11,6 +11,13 @@ def csv_find_row(file, columnNumber, searchNumber):
             return(csv_f.line_num)
             break
 
-def csv_import():
+def csvImport():
     for i in range:
         csv_f = csv.reader(open(loopfilename))
+
+def getListFromFile(filename):
+	files = []
+	with open("filelist.txt") as index:
+		for line in index:
+			files.append(line.strip('\n'))
+	files
