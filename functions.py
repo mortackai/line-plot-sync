d9afc11f2a7f48220a3086a77a8d2c842da3c4bb
Matt Raymond <mraymond@cryomech.com>
2020-10-08 16:37:34 -0400

    add mintemp, maxtemp, and avgtemp function

--- functions.py
+++ functions.py
@@ -21,3 +21,24 @@ def getListFromFile(filename):
 		for line in index:
 			files.append(line.strip('\n'))
 	return files
+
+# 2nd Stage Min Temp
+def minTemp(fileList, column):
+    for i, file in enumerate(fileList, start = 0):
+        print(fileList[i][0])
+        currentFile = csv.reader(open('test-curves/curve1.csv'))# + fileList[i][0]))
+        currentRow = fileList[i][1]
+        print(currentRow[0][0])
+        for row in currentFile:
+            if row[1] < x:
+                x = row[1]
+            else:
+                pass
+        print(x)
+            
+
+# 2nd Stage Average Temp
+#def avgTemp():
+
+# Return Max Temp
+#def maxTemp():
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 from functions import csvFindRow
 from functions import getListFromFile
+from functions import minTemp
 
 curveList = getListFromFile('filelist.txt')
 
@@ -12,4 +13,17 @@ for file in curveList:
     curvepointlist = []
     matrixA.append([file,csvFindRow('test-curves/' + file, 1, 290)])
 
-print(matrixA)
+
+with open('newcsv.csv', 'w', newline='') as f:
+    thewriter = csv.writer(f)
+
+    thewriter.writerow(['CH1_LOW', 'CH1_AVG', 'CH1_HIGH', 'CH1_MAX', 'CH2_LOW', 'CH2_AVG', 'CH2_HIGH', 'CH2_MAX'])
+    thewriter.writerow([])
+
+minTemp(matrixA,2)
+
+
+    
+#print(matrixA[0][0])
+#print(matrixA[0][1])
+
