7f242ff596b83b64ceaad3174aa350b8ec073be4
Matt Raymond <mraymond@cryomech.com>
2020-11-06 16:07:19 -0500

    sort of working - save point

--- functions.py
+++ functions.py
@@ -13,3 +13,25 @@ def get_list_from_file(filename):
 		for line in index:
 			files.append(line.strip('\n'))
 	return files
+
+def get_max_shift_index(curveList):
+
+    indexShift = []
+        
+    for file in curveList:
+        df = P.read_csv(file)
+        indexShift.append(csv_find_row(df,1,290))
+        return(max(indexShift))
+
+def combine_columns_of_files(maxIndexShift, curveList, columnName):
+    
+    csvHeaders = ["DateTime", "2ndStgSens", "1stStgSens", "Tube", "Null", "2ndStgHeat", "1stStgHeat", "AIO 1", "AIO 2", "AIO 3", "AIO 4", "DIO", "Relays", "V1", "V2", "V3"]
+    df2 = P.DataFrame()
+    
+    for file in curveList:
+        df = P.read_csv(file)                                               ### Read file name from list and import to dataframe
+        df.columns = csvHeaders                                             ### Rename column headers
+        df = df.shift(periods=maxIndexShift - (csv_find_row(df,1,290)))     ### shift index of dataframe to match one that started the latest
+        df2[file] = df[columnName]                                          ### append data from a row to the summary dataframe
+    return(df2)
+    
--- main.py
+++ main.py
@@ -2,27 +2,17 @@ import csv
 import pandas as P
 from functions import csv_find_row
 from functions import get_list_from_file
+from functions import get_max_shift_index
+from functions import combine_columns_of_files
 
 columnHeaders = ['CH1_LSD', 'CH1_AVG', 'CH1_USD', 'CH1_MAX', 'CH1_MIN', 'CH2_LSD', 'CH2_AVG', 'CH2_USD', 'CH2_MAX', 'CH2_MIN']
-csvHeaders = ["DateTime", "2ndStgSens", "1stStgSens", "Tube", "Null", "2ndStgHeat", "1stStgHeat", "AIO 1", "AIO 2", "AIO 3", "AIO 4", "DIO", "Relays", "V1", "V2", "V3"]
+
 
 curveList = get_list_from_file('filelist.txt')
-indexShift = []
-df2 = P.DataFrame()
 
-for file in curveList:
-    df = P.read_csv(file)
-    indexShift.append(csv_find_row(df,1,290))
-    
-maxIndexShift = max(indexShift)
+maxIndexShift = get_max_shift_index(curveList)
 
-for file in curveList:
-    df = P.read_csv(file)                                               ### Read file name from list and import to dataframe
-    df.columns = csvHeaders                                             ### Rename column headers
-    df = df.shift(periods=maxIndexShift - (csv_find_row(df,1,290)))     ### shift index of dataframe to match one that started the latest
-    df2[file] = df["2ndStgSens"]                          ### append data from a row to the summary dataframe
-
-df2.to_csv('test.csv', index=False)
+print(combine_columns_of_files(maxIndexShift, curveList, "2ndStgSens"))
 
 
 
